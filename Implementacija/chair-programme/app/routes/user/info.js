f05f37a3a077782eeda4fd5caf3e8130a593fa2e
Adna33 <adna1karkelja@gmail.com>
2017-05-21 21:52:35 +0200

    routes

--- Implementacija/chair-programme/app/routes/user/info.js
+++ Implementacija/chair-programme/app/routes/user/info.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	userService: Ember.inject.service(), 
+
+  model: function() {
+    return Ember.RSVP.hash({
+      speakers: this.get('userService').all()
+    });
+  }
 });
--- Implementacija/chair-programme/app/routes/user/show.js
+++ Implementacija/chair-programme/app/routes/user/show.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  userService: Ember.inject.service(), 
+
+     model: function(params) {
+        return Ember.RSVP.hash({
+            speaker: this.get('userService').getById(params.id)
+        });
+    }
 });
