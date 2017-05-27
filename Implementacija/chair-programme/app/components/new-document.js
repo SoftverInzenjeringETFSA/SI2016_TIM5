88f203bed963c2468625b5fdccf8e9a738d51e9f
Adna33 <adna1karkelja@gmail.com>
2017-05-27 20:00:48 +0200

    document validation

--- Implementacija/chair-programme/app/components/new-document.js
+++ Implementacija/chair-programme/app/components/new-document.js
@@ -1,12 +1,30 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 
 export default Ember.Component.extend({
+errors:DS.Errors.create(),
+
+validate: function(){
+  this.set('errors',DS.Errors.create());
+  if(this.get('dokument.name')==='' || this.get('dokument.name')===undefined){
+    this.get('errors').add('name','Input document name!');
+  }
+  if(this.get('dokument.documentLink')==='' || this.get('dokument.documentLink')===undefined){
+    this.get('errors').add('documentLink','Input document link!');
+  }
+  if(!this.get('terms')){
+    this.get('errors').add('checkbox','Terms and conditions not accepted!');
+  }
+
+  return this.get('errors.isEmpty');
+},
 
   actions:{
   submit: function(){
+    if(this.validate()){
       this.sendAction('action',this.dokument);
           console.log(this.dokument);
-          
+}
   }
 
 },
